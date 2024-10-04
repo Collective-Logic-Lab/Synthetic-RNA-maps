2889ea8b26255595652f2a839ef136b8a50a3653
alyoshavp <alyoshapetrov@icloud.com>
2024-10-04 11:16:29 -0700

    Update runRandomIC.sh

--- Code/runRandomIC.sh
+++ Code/runRandomIC.sh
@@ -87,8 +87,7 @@ while [ $run_ID -lt $num_simulations ]
 do
 echo $run_ID
 # Generate random input
-# python3 generate_randominput.py $gene_network
-python3 ./singleic_ip/generate_randominput.py $gene_network
+python3 generate_randominput.py $gene_network
 
 mkdir -p $output_directory/ic$run_ID
 
